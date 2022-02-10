e2826f1114b25e37ffae819e3ce99827c98c7722
Dominic <savagedialects@gmail.com>
2022-02-09 16:00:31 -0800

    final homework

---
+++ Lesson 07/03_functions-codealong/starter_code/script.js
@@ -0,0 +1,62 @@
+// Let's create 4 functions!
+
+// 1. Square of a number
+
+// Create a function called `squared` that takes one argument
+// The function will return the argument times itself (the square of the number)
+
+function squared(victim) {
+    victim = victim * victim;
+
+    return victim;
+} 
+
+console.log(squared(2));
+console.log(squared(5));
+console.log(squared(-3));
+
+// BONUS: Check if the argument is a number first! (Hint: conditionals - if statements)
+
+// 2. Get string length!
+
+// Create a function called `stringLength` that returns the length of a string (how many characters?)
+// So: stringLength("hello") would return 5
+
+function stringLength(victim){
+
+    let theLength = 0;
+    theLength = victim.length;
+    return theLength;
+}
+
+console.log(stringLength("testing"))
+console.log(stringLength("This is a longer string."))
+
+// 3. Add two numbers together
+
+// Create a function called addNumbers
+// Takes two arguments and returns the sum of those arguments
+function addNumbers(one, two){
+let output = one + two;
+return output;
+
+}
+
+
+console.log(addNumbers(4, 2));
+console.log(addNumbers(6, 3));
+
+// 4. Create a function called sayHello
+// Takes one argument
+// Returns a string that is the combination of "Hello, " + the argument passed into the function
+// So sayHello("Paul") would return "Hello, Paul";
+
+// console.log(sayHello("Paul"));
+
+function sayHello(theName){
+    let output = "Hello, " + theName;
+
+    return output;
+}
+
+console.log(sayHello("Paul"));
