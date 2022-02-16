de0967f693baf3b58587ed2cc94b581507bf8fdc
Dominic <savagedialects@gmail.com>
2022-02-15 19:11:44 -0800

    contractiond

--- Lesson 09/02_rock_paper_scissors/starter_code/game.js
+++ Lesson 09/02_rock_paper_scissors/starter_code/game.js
@@ -1,25 +1,114 @@
+document.addEventListener('DOMContentLoaded', function(event) {
 
-
-
-
-// Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
-function computerChoice() {
-
-};
-
-
-/* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
-
- */
-function compare(me, opponent) {
-
-};
-
-
-
-jQuery( document ).ready(function() {
-
-    // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
-
-
-});
+    // Create a function that determines the computer choice.  It should return one of the three values: 'rock', 'paper', or 'scissors'
+    function computerChoice() {
+    
+        // Pick a random number from 1->3
+        let computerChoiceInt = Math.random() * 3;
+    
+        // Make the number an Int
+        computerChoiceInt = Math.ceil(computerChoiceInt);
+    
+        // Assign a play value based on the number
+        if(computerChoiceInt == 1){
+            return "rock";
+        }else if(computerChoiceInt == 2){
+            return "paper";
+        }else{
+            return "scissors";
+        }
+    };
+    
+    
+    /* Create a function that compares the user's selection to the computer's selection.  It should then output the opponent's choice to #computerSelection and display who is the winner.
+    
+     */
+    function compare(me, opponent) {
+    
+        if(typeof me == "undefined" || typeof opponent == "undefined"){
+            winner = "tie";
+        }
+    
+        let winner = "";
+        me = me.trim().toLowerCase();
+        opponent = opponent.trim().toLowerCase();
+    
+        // Determine who wins
+        if(me == opponent){
+            // Tie
+            winner= "tie";
+        }else if(me == "rock"){
+            if(opponent == "paper"){
+                // opponent
+                winner="opponent";
+            }else{
+                // Me
+                winner="me";
+            }
+        }else if(me == "paper"){
+            if(opponent == "scissors"){
+                // opponent
+                winner="opponent";
+            }else{
+                // Me
+                winner="me";
+            }
+        }else{
+            if(opponent == "rock"){
+                // opponent
+                return "opponent";
+            }else{
+                // Me
+                winner="me";
+            }
+        }
+    
+        // Output to the screen
+        document.querySelector("#computerSelection").innerText =opponent;
+    
+        if(winner == "me"){
+            document.querySelector("#decision").innerText = "I WON!";
+        }else if(winner == "opponent"){
+            document.querySelector("#decision").innerText = "I LOST!";
+        }else{
+            document.querySelector("#decision").innerText = "It's a Tie!";
+        }
+    
+    
+    };
+    
+    
+        // Bind a click event onto each of the three buttons.  It should run the compare function and then put the result to the #decsision id in the html
+    
+        document.querySelector("#rock").addEventListener("click",function(e){
+    
+            e.preventDefault();
+    
+            let me = "rock";
+            let theComputerChoice = computerChoice();
+    
+            compare(me,theComputerChoice);
+        });
+    
+    
+        document.querySelector("#paper").addEventListener("click",function(e){
+    
+            e.preventDefault();
+    
+            let me = "paper";
+            let theComputerChoice = computerChoice();
+    
+            compare(me,theComputerChoice);
+        });
+    
+        document.querySelector("#scissors").addEventListener("click",function(e){
+    
+            e.preventDefault();
+    
+            let me = "scissors";
+            let theComputerChoice = computerChoice();
+    
+            compare(me,theComputerChoice);
+        });
+    
+    });
--- Lesson 09/03_function_contraction_factions/starter_code/js/controls.js
+++ Lesson 09/03_function_contraction_factions/starter_code/js/controls.js
@@ -1,44 +1,67 @@
-
-
 // This function will get the values of the two word forms, pass them to the compare function the update the display output
 
-function conjunctionFormChecking(){
+function conjunctionFormChecking(e){
+
+    e.preventDefault();
 
     // Create output variables
     var contractionFound = "";
     var logEntry = "";
 
     // Get words from search forms
+    let firstWord = document.querySelector("#firstWord").value.trim();
+    let secondWord = document.querySelector("#secondWord").value.trim();
 
     // Create phrase to test by combining wordOne and wordTwo with a space
+
+    let theTestPhrase = firstWord + " " + secondWord;
+
+
     // Note - there is already a function called "checkForConjunction(testPhrase)" that will return the conjunction OR false
 
+        let theResult = checkForConjunction(theTestPhrase);
 
         // Test phrase
-
-        // if Test Phrase returns false search phrase with words in reverse order
-
+        if(theResult == false){
+            // if Test Phrase returns false search phrase with words in reverse order
+            theTestPhrase = secondWord + " " + firstWord;
+            theResult = checkForConjunction(theTestPhrase);
+        }
 
     // If return is true
-
+    let output;
+    let logRecord;
+        if(theResult != false){
             // Set the output variable to the result
+            output = theResult;
 
             // Set the log variable to wordOne wordTwo and the contraction
+            logRecord = theResult + " " + firstWord + " " + secondWord;
 
-
+        }else{
             // Set the output variable to "None Found"
+            output = "None Found";
 
             // Set the log variable to wordOne wordTwo and no contraction wrapped in paragraph tags
+            logRecord = firstWord + " " + secondWord;
+        }
+
+           
 
 
         // Update contractionResult
+        document.getElementById('contractionResult').innerText = output;
 
         // Add wordOne + wordTwo + result to log
+        let theLogElement = document.createElement('p');
+        theLogElement.innerText = logRecord;
+        document.querySelector(".playAreaResultsLog").appendChild(theLogElement)
 
+    // Empty wordOne
+    document.getElementById('firstWord').value = "";
 
-        // Empty wordOne
-
-        // Empty wordTwo
+    // Empty wordTwo
+    document.getElementById('secondWord').value = "";
 
 
 } // end conjunctionFormChecking
@@ -47,15 +70,21 @@ function conjunctionFormChecking(){
 
 
 
-function resetAllForms(){
+function resetAllForms(e){
+
+    e.preventDefault();
 
     // Empty Result
+    document.getElementById('contractionResult').innerText = "";
 
     // Empty wordOne
+    document.getElementById('firstWord').value = "";
 
     // Empty wordTwo
+    document.getElementById('secondWord').value = "";
 
     // Empty the log
+    document.querySelector('.playAreaResultsLog').innerText ="";
 
 } // resetAllForms
 
@@ -65,10 +94,9 @@ function resetAllForms(){
 document.addEventListener('DOMContentLoaded', function(event) {
 
         // Bind conjunctionFormChecking to button
-
+        document.getElementById('btnCheckConjunction').addEventListener('click', conjunctionFormChecking);
 
         // Bind reset to button
-
-
+        document.querySelector("#btnReset").addEventListener('click',resetAllForms);
 
 });
